5470b4fc29bd43f5323d1a8975602a28d168b02d
Navneet <navn0306@gmail.com>
2021-12-24 20:15:47 +0530

    ADD: UC2, Ability to find frequency of words in a large paragraph phrase

--- src/main/java/com/bridge/hashtable/HashMapParagraph.java
+++ src/main/java/com/bridge/hashtable/HashMapParagraph.java
@@ -2,27 +2,21 @@ package com.bridge.hashtable;
 
 import java.util.ArrayList;
 
-public class HashMapParagraph<K> {
-    private int numOfBucket;
-    ArrayList<LinkedList<K>> myBucketArray;
+public class HashMapParagraph<K, V> {
+    private final int bucket;
+    ArrayList<LinkedList<K>> bucketArrayList;
 
-    public void HashMapParanoid() {
-        this.numOfBucket = 10;
-        this.myBucketArray = new ArrayList<>(numOfBucket);
-        for (int i = 0; i < numOfBucket; i++) {
-            this.myBucketArray.add(null);
+    public HashMapParagraph() {
+        this.bucket = 10;
+        this.bucketArrayList = new ArrayList<>(bucket);
+        for (int i = 0; i < bucket; i++) {
+            this.bucketArrayList.add(null);
         }
     }
 
-    private int getBucketIndex(K key) {
-        int hashCode = Math.abs(key.hashCode());
-        int index = hashCode % numOfBucket;
-        return index;
-    }
-
-    public <V> V get(K key) {
-        int index = this.getBucketIndex(key);  //2
-        LinkedList<K> linkedList = this.myBucketArray.get(index);
+    public V get(K key) {
+        int index = Math.abs(key.hashCode() % bucket);
+        LinkedList<K> linkedList = this.bucketArrayList.get(index);
         if (linkedList == null) {
             return null;
         }
@@ -30,18 +24,17 @@ public class HashMapParagraph<K> {
         return (mapNode == null) ? null : mapNode.getValue();
     }
 
-    public <V> void add(K key, V value) {
-        int index = this.getBucketIndex(key);
-        LinkedList<K> linkedList = this.myBucketArray.get(index);  // 2
-        if (linkedList == null)
+    public void add(K key, V value) {
+        int index = Math.abs(key.hashCode() % bucket);
+        LinkedList<K> linkedList = this.bucketArrayList.get(index);
+        if (linkedList == null) {
             linkedList = new LinkedList<>();
-        this.myBucketArray.set(index, linkedList);   // 2 -
+        }
+        this.bucketArrayList.set(index, linkedList);
         MyMapNode<K, V> mapNode = (MyMapNode<K, V>) linkedList.search(key);
         if (mapNode == null) {
             mapNode = new MyMapNode<>(key, value);
             linkedList.append(mapNode);
-        } else {
-            mapNode.setValue(value);
-        }
+        } else mapNode.setValue(value);
     }
 }
--- src/test/java/HashTable.java
+++ src/test/java/HashTable.java
@@ -22,4 +22,25 @@ public class HashTable {
         Integer expectedValue = 3;
         Assert.assertEquals(expectedValue, frequency);
     }
+
+    @Test
+    public void givenSentence_WhenLargeParaIsGiven_ShouldReturnWordFrequency() {
+
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words = sentence.toLowerCase().split(" ");
+        HashMapParagraph hashMapParagraph = new HashMapParagraph<>();
+        for (String word : words) {
+            Integer value = (Integer) hashMapParagraph.get(word);
+            if (value == null) {
+                value = 1;
+            } else {
+                value += 1;
+            }
+            hashMapParagraph.add(word, value);
+        }
+        Integer frequency = (Integer) hashMapParagraph.get("paranoid");
+        Integer expectedValue = 3;
+        Assert.assertEquals(expectedValue, frequency);
+
+    }
 }
